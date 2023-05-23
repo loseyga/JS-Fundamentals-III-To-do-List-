0623780247705962c58c1ee74c8bd873b9740769
loseyga <loseyga@gmail.com>
2023-05-23 14:54:23 -0600

    updated archive clear and clear all functionality

--- index.js
+++ index.js
@@ -18,8 +18,19 @@ document.addEventListener("click", archive);
 
 function archive(event) {
     let element = event.target;
+    let archiveNode = document.getElementById("archiveList");
+    let text = element.parentNode.innerText;
     if (element.className == "doneButton") {
-
+        let textNode = document.createTextNode(text);
+        let liNode = document.createElement("li");
+        liNode.setAttribute("class","archiveItem");
+        let trashButtonNode = document.createElement("input");
+        trashButtonNode.setAttribute("class", "trashButton");
+        trashButtonNode.setAttribute("type", "button");
+        trashButtonNode.setAttribute("value", "🗑️");
+        liNode.appendChild(textNode);
+        liNode.appendChild(trashButtonNode);
+        archiveNode.appendChild(liNode);
         element.parentNode.parentNode.removeChild(element.parentNode);
     }
 }
@@ -33,5 +44,24 @@ function clear(event) {
         if (getValue.value != "") {
             getValue.value = "";
         }
+    }
+}
+
+document.addEventListener("click", deleteLine);
+
+function deleteLine(event) {
+    let element = event.target;
+    if (element.className == "trashButton") {
+        element.parentNode.parentNode.removeChild(element.parentNode);
+    }
+}
+
+document.addEventListener("click", emptyTrash);
+
+function emptyTrash(event) {
+    let element = event.target;
+    let archiveListNode = document.getElementById("archiveList")
+    if (element.id == "clearAll") {
+        archiveListNode.innerHTML = "";
     }
 }
